c7531b8733fb60a98abd50fdbd3712529e223f21
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-05-20 20:20:12 -0600

    Gravedad lista

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,52 +4,51 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 5f;
-    public float gravity = -9.8f;
+    public float gravity = -9.81f;
     public Transform groundCheck;
-    public float groundDistance = 0.5f;
+    public float groundDistance = 0.2f;
     public LayerMask groundMask;
 
     private Rigidbody rb;
     private bool isGrounded;
     private Vector3 velocity;
 
-    private void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        CheckGrounded();
         Move();
         Jump();
-        ApplyGravity();
-    }
-
-    private void CheckGrounded()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
     }
 
     private void Move()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = transform.right * horizontalInput + transform.forward * verticalInput;
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        velocity.x = movement.x * speed;
+        velocity.z = movement.z * speed;
+        rb.velocity = velocity;
     }
 
     private void Jump()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0f)
+        {
+            velocity.y = -2f; // Restablece la velocidad vertical cuando toca el suelo
+        }
+
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity); // Cálculo del salto
         }
-    }
 
-    private void ApplyGravity()
-    {
-        velocity.y += gravity * Time.deltaTime;
-        rb.velocity = new Vector3(rb.velocity.x, velocity.y, rb.velocity.z);
+        velocity.y += gravity * Time.deltaTime; // Aplica la gravedad
+        rb.velocity = velocity;
     }
 }
