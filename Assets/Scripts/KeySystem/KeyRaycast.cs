937251f52b16b4ba646212554be2e634cb0213e9
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-05-21 23:46:46 -0600

    Agregando Note system

--- Assets/Scripts/KeySystem/KeyRaycast.cs
+++ Assets/Scripts/KeySystem/KeyRaycast.cs
@@ -11,29 +11,52 @@ namespace Keysystem
         [SerializeField] private LayerMask layerMaskInteract;
         [SerializeField] private string exluseLayerName = null;
         private KeyItemController raycastedObject;
+        private NoteController _noteController;
         [SerializeField] private KeyCode openDoorKey = KeyCode.Mouse0;
+        [SerializeField] private KeyCode interactNoteKey = KeyCode.Mouse0;
         [SerializeField] private Image crosshair = null;
         private bool isCrosshairActive;
         private bool doOnce;
 
         private string interactableTag = "InteractiveObject";
+        private string noteTag = "Note";
 
         private void Update()
         {
            
             RaycastHit hit;
             Vector3 fwd = transform.TransformDirection(Vector3.forward);
-            Debug.DrawRay(transform.position, fwd, Color.blue);
+            Debug.DrawRay(transform.position, fwd, Color.magenta);
 
             int mask = 1 << LayerMask.NameToLayer(exluseLayerName) | layerMaskInteract.value;
 
             if (Physics.Raycast(transform.position,fwd ,out hit, rayLength,mask)) 
             {
-                if(hit.collider.CompareTag(interactableTag)) 
+
+                var readbleItem = hit.collider.GetComponent<NoteController>();
+                if (readbleItem != null)
                 {
-                    if(!doOnce)
+
+                    _noteController = readbleItem;
+
+                }
+                else
+                {
+                    ClearNote();
+                }
+
+
+                
+
+
+                if (hit.collider.CompareTag(interactableTag)) 
+                {
+
+                    if (!doOnce)
                     {
+
                         raycastedObject = hit.collider.gameObject.GetComponent<KeyItemController>();
+                           
                         CrosshairChange(true);
 
                     }
@@ -45,17 +68,50 @@ namespace Keysystem
                     {
                         raycastedObject.ObjectInteraction();    
                     }
+
+                    
+
                 }
+
+                
             }
+
             else
             {
-                if(isCrosshairActive) 
+               
+                ClearNote() ;
+
+                if (isCrosshairActive) 
                 {
                     CrosshairChange(false);
                     doOnce = false;
                 }
             }
+
+                if (_noteController != null)
+                {
+                    if (Input.GetKeyDown(openDoorKey))
+                    {
+                        _noteController.ShowNote();
+                    }
+
+                }
+
+            
+
+
+
         }
+
+        void ClearNote()
+        {
+            if (_noteController != null)
+            {
+                _noteController = null;
+            }
+           
+        }
+
         void CrosshairChange(bool on)
         {
             if (on && !doOnce) 
