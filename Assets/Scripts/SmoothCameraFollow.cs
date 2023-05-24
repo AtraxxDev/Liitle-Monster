95889bb1be5a5b87fe45734460b95e029580c641
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-05-24 17:51:53 -0600

    Camera Collision

--- Assets/Scripts/SmoothCameraFollow.cs
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class SmoothCameraFollow : MonoBehaviour
 {
-    
+
 
     private Vector3 _offset;
     [SerializeField] private Transform target;
     [SerializeField] private float smoothTime;
     private Vector3 _currentVelocity = Vector3.zero;
-
-    
-
-    
 
     private void Awake()
     {
@@ -26,10 +22,40 @@ public class SmoothCameraFollow : MonoBehaviour
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
     }
 
-    private void LateUpdate()
+    private Vector3[] GetCameraCollisionPoints(Vector3 direction)
     {
-       
+        Vector3 position = target.position;
+        Vector3 center = position + direction * (Camera.main.nearClipPlane + 0.2f);
+
+        Vector3 right = transform.right * Camera.main.aspect;
+        Vector3 up = transform.up;
+
+        return new Vector3[]
+        {
+            center - right + up,
+            center + right + up,
+            center - right - up,
+            center + right - up
+        };
     }
 
-   
+    void LateUpdate()
+    {
+        Vector3 direction = transform.position - target.position;
+
+        RaycastHit hit;
+        float distance = direction.magnitude;
+        Vector3[] points = GetCameraCollisionPoints(direction.normalized);
+
+        foreach (Vector3 point in points)
+        {
+            if (Physics.Raycast(point, direction, out hit))
+            {
+                distance = Mathf.Min(hit.distance, distance);
+            }
+        }
+
+        transform.position = target.position + direction.normalized * distance;
+    }
+
 }
